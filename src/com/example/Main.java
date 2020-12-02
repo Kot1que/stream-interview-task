eab423a1f0e0dbb9a23864de511b7e63210ab595
Aleksei Meshcheriakov <aleksei_meshcheriakov@epam.com>
2020-12-02 11:34:36 +0300

    Reorder source

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -8,16 +8,16 @@ public class Main {
 
   public static void main(String[] args) {
     List<Hotel> hotels = Arrays.asList(
-        new Hotel("California", 100, RoomClass.FIVE, true),
+        new Hotel("California", 100, RoomClass.FIVE, false),
         new Hotel("Gold Zanzibar", 300, RoomClass.FIVE, true),
         new Hotel("Pushka Inn", 200, RoomClass.FIVE, true),
-        new Hotel("Club Aqua Gumbet", 40, RoomClass.TWO, true),
-        new Hotel("Cromwell Crown", 80, RoomClass.THREE, false),
-        new Hotel("Hello Jo", 25, RoomClass.ONE, false),
-        new Hotel("Corbigoe", 145, RoomClass.TWO, false),
+        new Hotel("Ali", 190, RoomClass.FIVE, true),
         new Hotel("Come Inn", 225, RoomClass.FOUR, true),
         new Hotel("Orfe", 60, RoomClass.THREE, false),
-        new Hotel("Ali", 190, RoomClass.FIVE, true)
+        new Hotel("Cromwell Crown", 80, RoomClass.THREE, false),
+        new Hotel("Corbigoe", 145, RoomClass.TWO, false),
+        new Hotel("Club Aqua Gumbet", 40, RoomClass.TWO, true),
+        new Hotel("Hello Jo", 25, RoomClass.ONE, false)
     );
 
     //  Task:
