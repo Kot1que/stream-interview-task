03d5a77c69065b51cfa4f8a57804fba6fdff7367
Aleksei Meshcheriakov <aleksei_meshcheriakov@epam.com>
2020-12-02 11:34:19 +0300

    Solution

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,21 +3,22 @@ package com.example;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
   public static void main(String[] args) {
-    List<Hotel> hotels = Arrays.asList(
-        new Hotel("California", 100, RoomClass.FIVE, true),
+    List<Hotel> hotelsSource = Arrays.asList(
+        new Hotel("California", 100, RoomClass.FIVE, false),
         new Hotel("Gold Zanzibar", 300, RoomClass.FIVE, true),
         new Hotel("Pushka Inn", 200, RoomClass.FIVE, true),
-        new Hotel("Club Aqua Gumbet", 40, RoomClass.TWO, true),
-        new Hotel("Cromwell Crown", 80, RoomClass.THREE, false),
-        new Hotel("Hello Jo", 25, RoomClass.ONE, false),
-        new Hotel("Corbigoe", 145, RoomClass.TWO, false),
+        new Hotel("Ali", 190, RoomClass.FIVE, true),
         new Hotel("Come Inn", 225, RoomClass.FOUR, true),
         new Hotel("Orfe", 60, RoomClass.THREE, false),
-        new Hotel("Ali", 190, RoomClass.FIVE, true)
+        new Hotel("Cromwell Crown", 80, RoomClass.THREE, false),
+        new Hotel("Corbigoe", 145, RoomClass.TWO, false),
+        new Hotel("Club Aqua Gumbet", 40, RoomClass.TWO, true),
+        new Hotel("Hello Jo", 25, RoomClass.ONE, false)
     );
 
     //  Task:
@@ -25,7 +26,18 @@ public class Main {
     //  Вывести по одному(любому) отелю для курящих из каждого класса
     //  Результат представить в виде Map<RoomClass, pricePerDay>
 
-    Map<RoomClass, Integer> result = hotels.stream();
+    Map<RoomClass, Integer> result = hotelsSource.stream()
+        .filter(Hotel::isSmoking)
+        .collect(
+            Collectors.groupingBy(
+                Hotel::getRoomClass,
+                Collectors.collectingAndThen(
+                    Collectors.toList(),
+                    hotels -> hotels.get(0).getPricePerDay()
+                )
+            )
+        );
+
 
     result.forEach((k, v) -> System.out.println(k + " " + v));
   }
